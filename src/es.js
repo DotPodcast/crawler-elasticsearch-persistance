a7029b16b239b2f2593975d915a4435db327d723
Jonathan Freeman <freethejazz@gmail.com>
2018-03-13 14:28:57 +0100

    Adds auth for elasticsearch

--- src/es.js
+++ src/es.js
@@ -2,8 +2,15 @@ import elasticsearch from 'elasticsearch';
 import config from './config';
 
 const client = new elasticsearch.Client({
-  host: `${config.get('es:host')}:${config.get('es:port')}`,
-  log: 'info',
+  host: [
+    {
+      host: config.get('es:host'),
+      port: config.get('es:port'),
+      log: 'info',
+      protocol: config.get('es:protocol'),
+      auth: `${config.get('es:username')}:${config.get('es:password')}`,
+    }
+  ]
 });
 
 const save = (index, type, id, body) => {
